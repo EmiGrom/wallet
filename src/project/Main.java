21871ae172fc58ab95028fc510dfe96ecf150d67
Drawdon93 <62105616+Drawdon93@users.noreply.github.com>
2020-03-18 14:54:59 +0100

    testowe

--- src/project/Main.java
+++ src/project/Main.java
@@ -1,6 +1,7 @@
 package project;
 
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {//psvm
 
@@ -9,8 +10,9 @@ public class Main {
         int typ = scanner.nextInt();
 
         System.out.println("ilość pieniędzy");
-        double money = scanner.nextDouble();
         Wallet wallet = null;
+        double money = scanner.nextDouble();
+
         switch (typ) {
             case 1:
                 wallet = new WalletPLN(money);
@@ -21,11 +23,36 @@ public class Main {
             case 3:
                 wallet = new WalletKILO(money);
                 break;
+            default:
+                System.out.println("wybór niemożliwy");
+                //System.out.println(wallet);
         }
         run(scanner, wallet);
-    }
 
+    }
     private static void run(Scanner scanner, Wallet wallet) {
+        Character operation;
+        System.out.println("Podaj rodzaj operacji: \n + wpłacanie \n - wypłacanie \n x koniec ");
+        operation = scanner.next().charAt(0);
+
+        switch (operation) {
+            case '+':
+                double moneyTym = scanner.nextDouble();
+                wallet.addMoney(moneyTym);
+                System.out.println(wallet);
+                break;
+            case '-':
+                moneyTym = scanner.nextDouble();
+                wallet.spendingMoney(moneyTym);
+                System.out.println(wallet);
+                break;
+            case 'x':
+                return;
+            default:
+                System.out.println("wybór niemożliwy");
+                System.out.println(wallet);
+        }
+        run(scanner, wallet);
 
 
     }
